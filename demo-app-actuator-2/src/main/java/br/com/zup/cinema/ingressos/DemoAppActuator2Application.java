80b426e913ad9c65483f746b52e5a4ce9240a29b
rafael.ponte@zup.com.br <rafael.ponte@zup.com.br>
2023-10-18 22:29:18 +0000

    stk-cli: create app initial commit.

---
+++ demo-app-actuator-2/src/main/java/br/com/zup/cinema/ingressos/DemoAppActuator2Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.cinema.ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppActuator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+}
